a1be25dc313e334e5ad0408c665b40a1625bc2f5
Evan Smal <esmal@tenstorrent.com>
2026-05-13 15:10:46 +0000

    Use C++17 now

--- tt_metal/jit_build/build.cpp
+++ tt_metal/jit_build/build.cpp
@@ -130,7 +130,7 @@ void JitBuildEnv::init(
 
     // Flags
     string common_flags =
-        "-std=c++20 -ftt-nttp -ftt-constinit -ftt-consteval"
+        "-std=c++17 -ftt-nttp -ftt-constinit -ftt-consteval"
         " -flto=auto -ffast-math -fno-exceptions"
         " -Wno-volatile -Wno-template-body ";
 
